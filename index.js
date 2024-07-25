c076d44ac87552cae403f8bbb7eee1439c92d247
WhoZihni <ahmedzihni27@gmail.com>
2024-07-25 10:08:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr 
-        console.log(count)
+    countEl.textContent = 0
+    count = 0
+    
 }
 
